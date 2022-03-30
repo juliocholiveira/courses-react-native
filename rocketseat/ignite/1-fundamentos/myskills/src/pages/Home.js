fe11655532c2c300e20d44088b5f547f15dad74a
Julio Oliveira <juliocholiveira@gmail.com>
2022-03-30 05:45:12 -0300

    feat: add skills list

--- rocketseat/ignite/1-fundamentos/myskills/src/pages/Home.js
+++ rocketseat/ignite/1-fundamentos/myskills/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -10,6 +10,18 @@ import {
 } from 'react-native';
 
 export function Home() {
+  const [myskill, setMyskill] = useState();
+  const [myskills, setMyskills] = useState([]);
+
+  const handleAddNewSkill = () => {
+    if (myskill === '') {
+      alert('My skill can not be null');
+      return;
+    }
+    setMyskills([...myskills, myskill]);
+    setMyskill('');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Wellcome, Júlio César!</Text>
@@ -17,12 +29,20 @@ export function Home() {
         style={styles.input}
         placeholder="My skills"
         placeholderTextColor="#555"
+        value={myskill}
+        onChangeText={setMyskill}
       />
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleAddNewSkill}>
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
 
       <Text style={[styles.title, {marginTop: 50}]}>My Skill</Text>
+
+      {myskills.map(skill => (
+        <TouchableOpacity key={skill} style={styles.buttonSkill}>
+          <Text style={styles.textSkill}>{skill}</Text>
+        </TouchableOpacity>
+      ))}
     </SafeAreaView>
   );
 }
@@ -59,5 +79,17 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  buttonSkill: {
+    backgroundColor: '#1F1E25',
+    marginVertical: 10,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
