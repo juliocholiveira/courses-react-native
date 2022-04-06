bdd1961092412a9f71a3ab19daf58e20b5da9c6d
Julio Oliveira <juliocholiveira@gmail.com>
2022-04-06 08:26:51 -0300

    add useEffect to greeting

--- rocketseat/ignite/1-fundamentos/myskills/src/pages/Home.js
+++ rocketseat/ignite/1-fundamentos/myskills/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -14,19 +14,33 @@ import {SkillCard} from '../components/SkillCard';
 export function Home() {
   const [myskill, setMyskill] = useState();
   const [myskills, setMyskills] = useState([]);
+  const [greeting, setGreeting] = useState('');
 
   const handleAddNewSkill = () => {
     if (myskill === '') {
       alert('My skill can not be null');
       return;
     }
-    setMyskills([...myskills, myskill]);
+    //setMyskills([...myskills, myskill]);
+    setMyskills(oldState => [...oldState, myskill]);
     setMyskill('');
   };
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+      setGreeting('Good Morning');
+    } else if (hour >= 12 && hour < 17) {
+      setGreeting('Good Afternoon');
+    } else {
+      setGreeting('Good night');
+    }
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Wellcome, Júlio César!</Text>
+      <Text style={styles.greeting}>{greeting}</Text>
       <TextInput
         style={styles.input}
         placeholder="My skills"
@@ -64,5 +78,9 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 32,
     borderRadius: 8,
+  },
+  greeting: {
+    color: '#FFF',
+    fontSize: 14,
   },
 });
