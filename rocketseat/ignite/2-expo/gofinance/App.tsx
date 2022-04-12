23cb9f5768215f20483b8ed2980cf05e58066747
Julio Oliveira <juliocholiveira@gmail.com>
2022-04-12 08:15:00 -0300

    add global styles

--- rocketseat/ignite/2-expo/gofinance/App.tsx
+++ rocketseat/ignite/2-expo/gofinance/App.tsx
@@ -1,8 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Dashboard } from "./src/screens/Dashboard";
 
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/theme";
+
 export default function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
