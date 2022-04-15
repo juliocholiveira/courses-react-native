481ac1afd72764576c8606f6c7ded2131d55d321
Julio Oliveira <juliocholiveira@gmail.com>
2022-04-15 09:47:44 -0300

    add header about user

---
+++ rocketseat/ignite/2-expo/gofinance/readme.md
@@ -0,0 +1,4 @@
+Instalar
+
+Libs utilizadas
+react-native-responsive-fontsize (https://www.npmjs.com/package/react-native-responsive-fontsize)
--- rocketseat/ignite/2-expo/gofinance/src/screens/Dashboard/index.tsx
+++ rocketseat/ignite/2-expo/gofinance/src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from "./styles";
 
 export const Dashboard = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/10158255?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Júlio</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
