1717bc9056b4d1b0baef091b7aeccbcd2e2cd47a
Julio Oliveira <juliocholiveira@gmail.com>
2022-04-22 06:20:32 -0300

    start register screen

--- rocketseat/ignite/2-expo/gofinance/App.tsx
+++ rocketseat/ignite/2-expo/gofinance/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
 import { ThemeProvider } from "styled-components";
 import AppLoading from "expo-app-loading";
 import {
@@ -24,7 +25,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
